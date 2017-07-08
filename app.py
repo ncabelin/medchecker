26641149c2fb0cb4ea699685d3b4e08c25020b57
Artemio Cabelin <artemiocabelin@gmail.com>
2017-07-08 15:00:48 -0700

    set app.run to default

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 def showHomepage():
 	return render_template('home.html')
 
+
 if __name__ == '__main__':
 	print('app started')
 	app.secret_key = 'secretkey'
-	app.run(host='0.0.0.0', port = 15000)
+	app.run(debug=True)
